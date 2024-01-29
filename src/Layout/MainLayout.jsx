0fa48cc0c76b1d7bbde7a99b8411cf661f02087d
nuaimhasan <nuaim.emanager@gmail.com>
2024-01-29 09:58:17 +0600

    components add

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -1,10 +1,14 @@
 import { Outlet } from "react-router-dom";
+import Header from "../components/Header/Header";
+import Footer from "../components/Footer/Footer";
 
 export default function MainLayout() {
   return (
     <>
       <div className="min-h-[70vh]">
+        <Header />
         <Outlet />
+        <Footer />
       </div>
     </>
   );
